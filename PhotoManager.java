6b0318a9de3180a4870fdfb3f2d4fea3eb9b9896
Reema1723 <444200778@student.ksu.edu.sa>
2025-04-26 16:00:28 +0300

    Update PhotoManager.java

--- PhotoManager.java
+++ PhotoManager.java
@@ -4,37 +4,24 @@ public class PhotoManager {
 
     LinkedList<Photo> photoList;
 
-  
+    
     public PhotoManager() {
         photoList = new LinkedList<Photo>();
+        
+    }
+    
+    public LinkedList<Photo> getPhotos() {
+        return photoList;
     }
 
-   
+    
     public void addPhoto(Photo P) {
         if (!isPhotoExists(P.getPath(), photoList)) {
             photoList.insert(P);
         }
     }
 
-  
-    private boolean isPhotoExists(String path, LinkedList<Photo> list) {
-        if (list.empty()) {
-            return false;
-        }
-
-        list.findFirst();
-        while (!list.last()) {
-            if (list.retrieve().getPath().equalsIgnoreCase(path)) {
-                return true;
-            }
-            list.findNext();
-        }
-
-      
-        return list.retrieve().getPath().equalsIgnoreCase(path);
-    }
-
-
+   
     public void deletePhoto(String path) {
         if (!isPhotoExists(path, photoList)) {
             return;
@@ -53,15 +40,30 @@ public class PhotoManager {
                 }
             }
 
-           
+            
             if (!removed && photoList.retrieve().getPath().equalsIgnoreCase(path)) {
                 photoList.remove();
             }
         }
     }
 
-  
-    public LinkedList<Photo> getPhotos() {
-        return photoList;
+    
+    private boolean isPhotoExists(String path, LinkedList<Photo> list) {
+        if (list.empty()) {
+            return false;
+        }
+
+        list.findFirst();
+        while (!list.last()) {
+            if (list.retrieve().getPath().equalsIgnoreCase(path)) {
+                return true;
+            }
+            list.findNext();
+        }
+
+        
+        return list.retrieve().getPath().equalsIgnoreCase(path);
     }
+
+    
 }
