a212a9bca48bf1572270682457f7609ea9b1b63f
layan77i <444201136@student.ksu.edu.sa>
2025-04-26 14:16:54 +0300

    Update PhotoManager.java

--- PhotoManager.java
+++ PhotoManager.java
@@ -4,19 +4,19 @@ public class PhotoManager {
 
     LinkedList<Photo> photoList;
 
-    // Constructor
+  
     public PhotoManager() {
         photoList = new LinkedList<Photo>();
     }
 
-    // Method to add a photo
+   
     public void addPhoto(Photo newPhoto) {
         if (!isPhotoExists(newPhoto.getPath(), photoList)) {
             photoList.insert(newPhoto);
         }
     }
 
-    // Helper method to check if a photo exists
+  
     private boolean isPhotoExists(String path, LinkedList<Photo> list) {
         if (list.empty()) {
             return false;
@@ -30,11 +30,11 @@ public class PhotoManager {
             list.findNext();
         }
 
-        // Final check for the last item
+      
         return list.retrieve().getPath().equalsIgnoreCase(path);
     }
 
-    // Method to delete a photo by path
+
     public void deletePhoto(String path) {
         if (!isPhotoExists(path, photoList)) {
             return;
@@ -53,14 +53,14 @@ public class PhotoManager {
                 }
             }
 
-            // Check the last item if not already removed
+           
             if (!removed && photoList.retrieve().getPath().equalsIgnoreCase(path)) {
                 photoList.remove();
             }
         }
     }
 
-    // Return the list of photos
+  
     public LinkedList<Photo> getPhotos() {
         return photoList;
     }
